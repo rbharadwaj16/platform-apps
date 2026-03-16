0c98a2c6b442d78e9f8cc33a15fb5ff1f9ea01e9
rbharadwaj16 <rbharadwaj16@gmail.com>
2026-03-16 23:18:23 +0530

    Refactor translate endpoint to include extracted data from AI intent

--- translator-service/app/main.py
+++ translator-service/app/main.py
@@ -6,13 +6,27 @@ app = FastAPI()
 def health():
     return {"status": "ok"}
 
+def extract_intent_from_ai(user_request):
+    return {
+        "resource_type": "storage_account",
+        "region": "westeurope",
+        "resource_group": "rg-app-prod",
+        "resource_name": "stappfiles001",
+        "sku": "Standard_LRS",
+        "environment": "prod"
+    }
+
+
 @app.post("/translate")
 def translate(request_body: dict):
     if "request" not in request_body:
-        raise HTTPException (status_code=400, detail="Missing 'request' field")
+        raise HTTPException(status_code=400, detail="Missing 'request' in body")
     
     user_request = request_body["request"]
 
+    extracted_data = extract_intent_from_ai(user_request)
+
     return {
-        "user_request": user_request
+        "user_request": user_request,
+        "extracted_data": extracted_data
     }
